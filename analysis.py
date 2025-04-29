8eeac0c34060dc4abbbbde2184e1949600c105d9
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2025-04-29 09:11:22 -0700

    first commit

---
+++ analysis.py
@@ -0,0 +1,81 @@
+import time
+import numpy as np
+import pandas as pd
+import io
+import requests
+from functions import pitcher_req, batter_request
+import warnings
+warnings.simplefilter('ignore', FutureWarning)
+
+oppo_hand = {
+    "R": "L",
+    "L": "R"
+}
+
+names = pd.read_csv("names.csv", index_col="player_id", encoding="utf-8")
+pitch_types = {
+    "FF": "Four Seamer",
+    "SI": "Sinker",
+    "FC": "Cutter",
+    "CH": "Changeup",
+    "FS": "Split-finger",
+    "FO": "Forkball",
+    "SC": "Screwball",
+    "CU": "Curveball",
+    "KC": "Knuckle Curve",
+    "CS": "Slow Curve",
+    "SL": "Slider",
+    "ST": "Sweeper",
+    "SV": "Slurve",
+    "KN": "Knuckleball",
+    "EP": "Eephus",
+    "FA": "Other",
+    "IN": "Intentional Ball",
+    "PO": "Pitchout"
+}
+
+def run(hitters, pitcher, team, phand) -> pd.DataFrame:
+    pitcher = pitcher_req(pitcher, ["2025"])
+    pname = pitcher['player_name'].iloc[0]
+    pitcher["pitch_type"] = pitcher["pitch_type"].apply(lambda x: pitch_types[x])
+    pitcher.index = pitcher["pitch_type"]
+
+    result = pd.DataFrame()
+    for playerid in hitters:
+        time.sleep(0.1)
+        hitter = batter_request(playerid, ["2025", "2024"], phand)
+
+        edited_table = hitter.loc[:, ["pitches", "pitch_type", "hits", "singles", "doubles", "triples", "hrs", "pa"]].fillna(0)
+        edited_table["pitch_type"] = edited_table["pitch_type"].apply(lambda x: pitch_types[x])
+        edited_table = edited_table.groupby("pitch_type").aggregate(sum)
+
+        edited_table["hit/pitch"] = edited_table["hits"] / edited_table["pitches"]
+        edited_table["bases"] = edited_table["singles"] + edited_table["doubles"] * 2 + edited_table["triples"]*3 + edited_table["hrs"] * 4
+        edited_table["bases/pitch"] = edited_table["bases"] / edited_table["pitches"]
+        # edited_table = edited_table.sort_values("bases/pitch", ascending=False)
+
+        average_pa = edited_table["pitches"].sum() / edited_table["pa"].sum()
+        avghp = edited_table["hits"].sum() / edited_table["pitches"].sum() * average_pa * 5
+        avgbp = edited_table["bases"].sum() / edited_table["pitches"].sum() * average_pa * 5
+
+        batter_hand = names.loc[int(playerid)]["bat_hand"]
+        if batter_hand == "S":
+            batter_hand = oppo_hand[phand]
+        
+        pitches = pitcher[pitcher["batter_hand"] == batter_hand]["pitches"]
+        perc = pitches / pitches.sum()
+        perc = perc * average_pa * 5
+        hp = perc * edited_table["hit/pitch"]
+        bp = perc * edited_table["bases/pitch"]
+        print(f"{names.loc[int(playerid)]["player_name"]} will get {hp.sum()} hits and {bp.sum()} bases per 5 ab")
+        result = pd.concat([result, pd.DataFrame([{
+            "Name": names.loc[int(playerid)]["player_name"],
+            "Team": team,
+            "Opposing Pitcher": pname,
+            "H / 5ab": hp.sum(),
+            "B / 5ab": bp.sum(),
+            "AVG H / 5ab": avghp,
+            "AVG B / 5ab": avgbp,
+            "PA": edited_table["pa"].sum()
+        }])])
+    return result
---
+++ get_game.py
@@ -0,0 +1,101 @@
+import json
+from functions import session
+from analysis import run
+import pandas as pd
+import plotly.express as px
+from datetime import datetime
+import sys
+
+date  = datetime.now().strftime("%Y-%m-%d")
+
+if sys.argv[1] and sys.argv[1] != "":
+    date = sys.argv[1]
+
+greq = session.get(f"https://baseballsavant.mlb.com/schedule?date={date}")
+day = json.loads(greq.text)
+
+result = pd.DataFrame()
+
+for game in day["schedule"]["dates"][0]["games"]:
+    print(f"Starting {game["teams"]["away"]["team"]["clubName"]} @ {game["teams"]["home"]["team"]["clubName"]}")
+    res = session.get(f"https://baseballsavant.mlb.com/preview?game_pk={game["gamePk"]}&game_date={game["officialDate"]}")
+    home_abbr = game["teams"]["home"]["team"]["abbreviation"]
+    away_abbr = game["teams"]["away"]["team"]["abbreviation"]
+    data = res.text
+
+    start_index = data.find("var teams = ")
+
+    end_index = data.find(";", start_index + 12)
+
+    data = data[start_index + 12:end_index]
+
+    parsed = json.loads(data)
+
+    home_hitters = []
+
+    for i in parsed["home"]["roster"]["hitters"]:
+        try:
+            home_hitters.append(str(i["player_id"]))
+        except:
+            try:
+                print(f"Error with player {i["person"]["fullName"]}")
+            except:
+                print("Error with random player")
+
+    away_hitters = []
+
+    for i in parsed["away"]["roster"]["hitters"]:
+        try:
+            away_hitters.append(str(i["player_id"]))
+        except:
+            try:
+                print(f"Error with player {i["person"]["fullName"]}")
+            except:
+                print("Error with random player")
+
+    home_prob = ""
+    home_prob_hand = ""
+
+    for i in parsed["home"]["roster"]["pitchers"]:
+        try:
+            if i["isPlaying"]:
+                home_prob = str(i["player_id"])
+                home_prob_hand = str(i["pitch_hand"])
+                break
+        except:
+            continue
+
+    away_prob = ""
+    away_prob_hand = ""
+
+    for i in parsed["away"]["roster"]["pitchers"]:
+        try:
+            if i["isPlaying"]:
+                away_prob = str(i["player_id"])
+                away_prob_hand = str(i["pitch_hand"])
+                break
+        except:
+            continue
+    try:
+        if(away_prob != ""):
+            result = pd.concat([result, run(home_hitters, away_prob, home_abbr, away_prob_hand)], ignore_index=True)
+    except Exception as e:
+        print(f"Error with {game["teams"]["home"]["team"]["clubName"]} batters\n{repr(e)}")
+
+    try:
+        if(home_prob != ""):
+            result = pd.concat([result, run(away_hitters, home_prob, away_abbr, home_prob_hand)], ignore_index=True)
+    except Exception as e:
+        print(f"Error with {game["teams"]["away"]["team"]["clubName"]} batters\n{repr(e)}")
+    print(result)
+
+    result.to_csv("result.csv", encoding='utf-16')
+
+result["Net Matchup vs Avg (hits)"] = result["H / 5ab"] - result["AVG H / 5ab"]
+result["Net Matchup vs Avg (bases)"] = result["B / 5ab"] - result["AVG B / 5ab"]
+result.to_csv("result.csv", encoding='utf-16')
+
+result = result[result["PA"] > 50]
+
+pot = px.scatter(result, x="H / 5ab", y="B / 5ab", color='PA', hover_data={"Name": True, "Net Matchup vs Avg (hits)" : True, "Net Matchup vs Avg (bases)" : True, "Opposing Pitcher" : True,})
+pot.write_html("index.html", include_plotlyjs="cdn")
